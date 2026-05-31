2e634114d708faed5639486ca2cc47c3935f7de5
thewhiteneegro <nanakhayyy@gmail.com>
2026-05-31 21:07:07 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,6 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Sonla Benyah Ocran
+# STUDENT ID: 2425402861
+# Email: nanakhayyy@gmail.com
+# Grader: Michael David Scott
+#
+# On my honor, Sonla Benyah Ocran, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print my initials SBO
+
+    print()
+    print("...SBO")
+    print()
+
+    print("...SSSSSSSSSSSS....BBBBBBBBBBBB....OOOOOOOOOOOO")
+    print("...SSSSSSSSSSSS....BBBBBBBBBBBB....OOOOOOOOOOOO")
+    print("...SSS.............BBB.......BB....OOO......OOO")
+    print("...SSS.............BBB.......BB....OOO......OOO")
+    print("...SSSSSSSSSSSS....BBBBBBBBBBBB....OOO......OOO")
+    print("............SSS....BBB.......BB....OOO......OOO")
+    print("............SSS....BBB.......BB....OOO......OOO")
+    print("...SSSSSSSSSSSS.**.BBBBBBBBBBBB.**.OOOOOOOOOOOO")
+    print("...SSSSSSSSSSSS.**.BBBBBBBBBBBB.**.OOOOOOOOOOOO")
+    print("................................................")
+
+    print()
 
 
 
